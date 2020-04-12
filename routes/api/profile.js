65fff567187af0caa4d9387fdbacde8d41598894
Rafael Parola <rafaelparola@gmail.com>
2020-04-12 19:21:13 -0300

    update profile experiences working

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -116,16 +116,81 @@ router.get('/', async (req, res) => {
 // @route   GET api/profile/user/:user_id
 // @desc    Get profile by user ID
 // @access  Public
-router.get('/user/:user_id', async (req, res) => {
+router.get('/user/:user_id', auth, async (req, res) => {
     try {
-        const profile = await Profile.findOne({ user: req.params.user_id }).populate('user',['name', 'avatar']);
-        if(!profile) return res.status(400).json({ msg: 'There is no profile for this user' });
-        res.json(profile);
+        //To do later -- remove users posts
+
+        //Remove profile
+        await Profile.findOneAndRemove({ user: req.params.user_id });
+        //Remove user
+        await User.findOneAndRemove({ _id: req.params.user_id });
+        res.json({msg: "User deleted"});
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Server error');
     }
 })
 
+// @route   DELETE api/profile
+// @desc    Delete profile, user and post
+// @access  Private
+router.delete('/', async (req, res) => {
+    try {
+        const profiles = await Profile.find().populate('user',['name', 'avatar']);
+        res.json("User deleted");
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+})
+
+// @route   PUT api/profile/experience
+// @desc    Add profile experience
+// @access  Private
+router.put('/experience', auth, async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+        title,  
+        company,
+        location,
+        from,
+        to,
+        current,
+        description
+    } = req.body;
+
+    const newExp = {
+        title,
+        company,
+        location,
+        from,
+        to,
+        current,
+        description
+    }
+
+    try {
+        const profile = await Profile.findOne({user: req.user.id});
+        profile.experience.unshift(newExp);
+        await profile.save();
+        res.json(profile);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+
+})
+
+/*
+    O put nao funcionava com esses checks, ficava preso e nao retornava nada
+  [auth, [
+    check('title', 'Title is required').not().isEmpty,
+    check('company', 'Company is required').not().isEmpty,
+    check('from', 'From date is required').not().isEmpty
+]] */
 
 module.exports = router;
